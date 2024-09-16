5193848e2519f687cff1a973763f1938663cf391
Ayobami Ajayi <lynxdev32@gmail.com>
2024-09-16 09:00:30 +0100

    add ignore esLint during build

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,11 @@ const nextConfig = {
     // !! WARN !!
     ignoreBuildErrors: true,
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
