c69ab37c87567f491db56b39692759b55b696eca
Ayobami Ajayi <lynxdev32@gmail.com>
2024-10-14 17:45:44 +0100

    fix: hover button in dark mode

--- src/app/blog/components/ShareOptions.tsx
+++ src/app/blog/components/ShareOptions.tsx
@@ -53,14 +53,17 @@ const ShareOptions = ({
           className='absolute right-0 top-[110%] z-20 flex w-[13rem] flex-col gap-1 rounded-md border border-gray-200 bg-white p-1.5 shadow-lg *:rounded *:!px-2 *:text-left *:flex *:items-center *:gap-3 *:!py-2 dark:border-[#3a3a3a] dark:bg-darkBg'
           ref={shareMenuRef}
         >
-          <button className='hover:bg-gray-200' onClick={copyLink}>
+          <button
+            className='hover:bg-gray-200 dark:hover:bg-[#262626]'
+            onClick={copyLink}
+          >
             <FaLink className='size-5 text-primary dark:text-darkPrimary' />
             <p>Copy Link</p>
           </button>
           <TwitterShareButton
             url={`https://coffee-and-convos.vercel.app/blog/${publishLink}`}
             title={title}
-            className='hover:!bg-gray-200'
+            className='hover:!bg-gray-200 dark:hover:!bg-[#262626]'
           >
             <FaXTwitter className='size-5 text-primary dark:text-darkPrimary' />
             <p>Share to X</p>
@@ -68,7 +71,7 @@ const ShareOptions = ({
           <WhatsappShareButton
             url={`https://coffee-and-convos.vercel.app/blog/${publishLink}`}
             title={title}
-            className='hover:!bg-gray-200'
+            className='hover:!bg-gray-200 dark:hover:!bg-[#262626]'
           >
             <FaWhatsapp className='size-5 text-primary dark:text-darkPrimary' />
             <p>Share to Whatsapp</p>
@@ -76,7 +79,7 @@ const ShareOptions = ({
           <FacebookShareButton
             url={`https://coffee-and-convos.vercel.app/blog/${publishLink}`}
             title={title}
-            className='hover:!bg-gray-200'
+            className='hover:!bg-gray-200 dark:hover:!bg-[#262626]'
           >
             <FaFacebook className='size-5 text-primary dark:text-darkPrimary' />
             <p>Share to Facebook</p>
@@ -85,7 +88,7 @@ const ShareOptions = ({
             url={`https://coffee-and-convos.vercel.app/blog/${publishLink}`}
             source={`https://coffee-and-convos.vercel.app/blog/${publishLink}`}
             title={title}
-            className='hover:!bg-gray-200'
+            className='hover:!bg-gray-200 dark:hover:!bg-[#262626]'
           >
             <FaLinkedinIn className='size-5 text-primary dark:text-darkPrimary' />
             <p>Share to Linkedin</p>
@@ -93,7 +96,7 @@ const ShareOptions = ({
           <RedditShareButton
             url={`https://coffee-and-convos.vercel.app/blog/${publishLink}`}
             title={title}
-            className='hover:!bg-gray-200'
+            className='hover:!bg-gray-200 dark:hover:!bg-[#262626]'
           >
             <FaRedditAlien className='size-5 text-primary dark:text-darkPrimary' />
             <p>Share to Reddit</p>
