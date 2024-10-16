4d3b51ca25ca5d05666eb5e178fec289db260cc4
Ayobami Ajayi <lynxdev32@gmail.com>
2024-10-16 13:42:16 +0100

    add: vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import AppProvider from "./contexts/AppContext";
+import { Analytics } from "@vercel/analytics/react";
 import Navbar from "./components/Navbar";
 import { Toaster } from "sonner";
 import WarningModalProvider from "./contexts/WarningModalContext";
@@ -32,6 +33,7 @@ export default function RootLayout({
             <Toaster richColors position='top-right' />
             {children}
           </WarningModalProvider>
+          <Analytics />
         </body>
       </AppProvider>
     </html>
